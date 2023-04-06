15143c7044fc7a0baa8773f1854fcc0be2f51a88
Elvis O <elvis.osujic@gmail.com>
2023-04-06 22:39:14 +0100

    Routing and login validating

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,20 @@
+// Packages
 import express from "express";
 import { json } from "body-parser";
+
+// Routes
+import { currentUserRouter } from "./routes/current-user";
+import { signoutRouter } from "./routes/signout";
+import { signinRouter } from "./routes/signin";
+import { signupRouter } from "./routes/signup";
 
 const app = express();
 app.use(json());
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("Hey user");
-});
+app.use(currentUserRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
+app.use(signinRouter);
 
 app.listen(2000, () => {
   console.log("Listening on port 2000!");
