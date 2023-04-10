27ca60f14247b6515eaeb7bdfa05c050f7a1467f
Elvis O <elvis.osujic@gmail.com>
2023-04-10 13:37:17 +0100

    Added sessions and JWT

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
 import { RequestValidationError } from "../errors/request-validation";
 import { DatabaseConnectionError } from "../errors/database-connection-error";
 import { User } from "../models/user";
@@ -36,6 +37,20 @@ router.post(
       password,
     });
     await user.save();
+
+    //Generate JSON Web token
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    // Store it on sessiom
+    req.session = {
+      jwt: userJwt,
+    };
 
     res.status(201).send(user);
   }
