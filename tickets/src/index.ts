3845b84ff0e8578a60fcbfea9669165bd74f465d
Elvis O <elvis.osujic@gmail.com>
2023-04-21 13:55:40 +0100

    Added tickets service with working db.

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -8,9 +8,12 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
+  }
   try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
-    console.log("AUTH connected to DB");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("TICKETS db connected to DB");
   } catch (err) {
     console.error(err);
   }
