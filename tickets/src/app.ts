52d34eba0428aeb3f991a12bcc5ed086fe7ef3f1
Elvis O <elvis.osujic@gmail.com>
2023-04-24 13:14:41 +0100

    Tickets test

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,7 +3,8 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
-import { errorHandler, NotFoundError } from "@ticketifyorg/common";
+import { errorHandler, NotFoundError, currentUser } from "@ticketifyorg/common";
+import { createTicketRouter } from "./routes/new";
 // Routes
 
 const app = express();
@@ -15,6 +16,10 @@ app.use(
     secure: process.env.NODE_ENV !== "test",
   })
 );
+
+app.use(currentUser);
+
+app.use(createTicketRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
